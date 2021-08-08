6a09b85091c3e65abce3e1ca098e2c18461e2e70
Iskander-alam <iskanderalam464@gmail.com>
2021-08-08 18:44:07 +0600

    first commit'

---
+++ index.js
@@ -0,0 +1,195 @@
+// // Addition
+// var age =25;
+// var Age =15;
+// var ages= age+Age;
+// console.log(ages);
+// // subtraction
+// var age = 25;
+// var ages= 7;
+// var Age=age-ages;
+// console.log(Age);
+// // multifliation
+// var age = 27;
+// var ages =2;
+// var Age =age+ages;
+// console.log(Age);
+// // Division
+// var age =23;
+// var ages =2;
+// var Age =age/ages;
+// console.log(Age);
+
+
+// //Math operation
+// var age =24;
+// var ages =3;
+// var Age=age+ages;
+// console.log(Age);
+
+// var number=22;
+// var numbers=2;
+// var num=number-numbers;
+// console.log(num);
+
+// var mul=23;
+// var mel=3;
+// var mal =mul*mel;
+// console.log(mal)
+
+// var div=4;
+// var dive=2;
+// var dev =div/dive;
+// console.log(dev)
+
+// var age=23;
+// var ages=2;
+// age *=2;
+// console.log(age);
+
+// var firstName="Iskadner";
+// var lastName="Alam";
+// var fullName=firstName+ ' '+lastName;
+// console.log(fullName);
+
+// let face ='iskander';
+// let faces='alam';
+// let fas=face+ ' ' +faces;
+// console.log(fas);
+
+// var age =24;
+// var ages =15;
+// var Age=age+ages;
+
+// console.log(Age);
+
+// var age = 23;
+// var ages=5;
+// var Age=age-ages;
+// console.log(Age);
+
+// var age =19;
+// var ages= 3;
+// var Age=age*ages;
+// console.log(Age);
+
+// var age = 60;
+// var ages= 2;
+// var Age=age/ages;
+// console.log(Age);
+
+// var firsName='Iskander';
+// var lastName='Alam';
+// var fullName =firstName +' '+ lastName;
+// console.log(fullName);
+
+
+// // function factorial(n)
+// // {
+// //           if (n==1)
+// //              return 1;
+           
+// //              return n*factorial(n-1);
+// // }
+
+// // const myFactorial=factiorial(5);
+// // console.log(myFactorial);
+
+// var dates =20;
+
+// dates-=5;
+// dates--;
+// console.log(dates);
+
+var task="34";
+var taks="34";
+var fullnum=task+taks;
+console.log(fullnum);
+
+var numers=34;
+console.log(typeof numers);
+
+var summer=true;
+console.log(typeof summer);
+
+var today= new Date();
+console.log(today);
+
+var orange=7;
+var hungryPerson=3;
+var remaining=orange%hungryPerson;
+console.log(remaining);
+
+var age=21;
+age +=8;
+console.log(typeof age);
+
+// Array ---------
+
+var friendAge =[11, 20, 14];
+console.log(friendAge);
+
+var name =['babu', 'manik','borhan']
+var count=name.length;
+console.log(count);
+
+//Array index
+
+var pages =['font','middle','last','presentlast','pro','doed']
+var page =pages.indexOf('pro');
+console.log(page);
+
+// Array usisgn push
+
+var names = ['kalam','salam','palam']
+var nam =names;
+nam.push('kolim','jamal');
+console.log(names);
+
+// Array using pop
+
+var name =['kola','mola','sola','jola']
+var names=name;
+name.pop();
+console.log(names);
+
+//Array usign shift
+var month =['jan','feb','marc','apr']
+var months=month;
+month.shift();
+console.log(month);
+
+//Array usign unshift
+var month =['jan','feb','marc','apr']
+var months=month;
+month.unshift('none');
+console.log(month);
+
+// comparison operator
+console.log(5>6);
+console.log(5<8);
+console.log(5 == 6);
+console.log(7 == 7);
+console.log(5 != 6);
+console.log(5 != 5);
+
+var name = 'Iskander';
+var names ="My name is"+ " " + name;
+
+console.log(names);
+
+// multiple array join by using
+
+var a=[1,2];
+var b=[3,4];
+var c=a+b.concat(5,5,);
+console.log(c);
+
+// while loop 
+
+const list = ['a', 'b', 'c']
+let i = 0
+while (i < list.length) {
+console.log(list[i]) //value
+console.log(i) //index
+i = i + 1
+}
